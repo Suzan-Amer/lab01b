9272bb5cb70d463f715340b635dccfbad209ce6e
suzan-amer <amersuzan579@gmail.com>
2021-04-25 15:20:17 +0300

    hi

---
+++ app.js
@@ -0,0 +1,14 @@
+let userName = prompt('can you enter your name ,plz?');
+console.log ('username : ' +userName);
+
+let age = prompt('can you enter your age , plz?');
+console.log ('age : '+age);
+
+let favSport = prompt('what\'s your fav sport?');
+console.log ('favSport : ' +favSport);
+ 
+let favPlayer = prompt('who is your fav player?');
+console.log( 'favPlayer : ' +favPlayer);
+
+alert ('hallo ' + userName +' your age is '+ age + ' your fav sport is  ' + favSport +
+ ' your fav player is ' + favPlayer)
